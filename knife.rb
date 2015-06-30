b610d46cea90371a75899ad8b5b71bd070ba0c26
Justin Seubert <dude051@gmail.com>
2015-06-30 13:27:05 -0500

    Adding new method to write keys for knife.rb

--- knife.rb
+++ knife.rb
@@ -2,9 +2,9 @@ current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
 node_name                "#{ENV['CIRCLE_CI_MACHINE_USER_CLIENT_NAME']}"
-client_key               ENV['CIRCLE_CI_MACHINE_USER_CLIENT_KEY']
+client_key               "#{current_dir}/client.pem"
 validation_client_name   "#{ENV['CIRCLE_CI_CHEF_ORG_CLIENT']}"
-validation_key           ENV['CIRCLE_CI_CHEF_ORG_KEY']
+validation_key           "#{current_dir}/validator.pem"
 chef_server_url          "https://api.opscode.com/organizations/#{ENV['CIRCLE_CI_CHEF_ORG']}"
 cache_type               'BasicFile'
 cache_options(path:      "#{ENV['HOME']}/.chef/checksums")
